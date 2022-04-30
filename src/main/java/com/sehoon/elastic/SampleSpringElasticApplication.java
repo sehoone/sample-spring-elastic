634247ba4d7e22d74d8a545762eecbc688636285
SeHoon <sehoon1989@gmail.com>
2022-04-30 16:49:45 +0900

    init

---
+++ src/main/java/com/sehoon/elastic/SampleSpringElasticApplication.java
@@ -0,0 +1,13 @@
+package com.sehoon.elastic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringElasticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringElasticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sehoon/elastic/SampleSpringElasticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sehoon.elastic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringElasticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
